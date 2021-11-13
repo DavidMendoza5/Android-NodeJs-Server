558a03f5fed7dcbfcdbb09735a00e3de9ba90178
David <cruz-azul99@hotmail.com>
2021-11-13 13:56:43 -0600

    Message changed

--- src/db/repository/abstract.repository.js
+++ src/db/repository/abstract.repository.js
@@ -32,7 +32,7 @@ const deleteData = async (schema, id) => {
     const data = await schema.findOneAndDelete({ _id:id })
     return data
   } catch(err) {
-    throw new Error('Error al eliminar en la base de datos')
+    throw new Error('Error al eliminar el dato en la base de datos')
   }
 }
 
